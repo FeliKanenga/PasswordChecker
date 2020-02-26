c3d8b93d0e450c958b862a42c1662100db54bafa
feliKanenga <felicia.kanenga@umuzi.org>
2020-02-26 15:41:49 +0200

    PasswordChecker

--- src/test/java/Password_CheckerTest.java
+++ src/test/java/Password_CheckerTest.java
@@ -1,18 +1,17 @@
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class Password_CheckerTest {
 
-    public class Password_CheckerTest {
-
-        @org.junit.Test
+        @Test
         public void passwordIsValid() throws PasswordException {
             Assert.assertTrue(Password_Checker.passwordIsValid("passsssssA\"3"));
         }
 
-        @org.junit.Test
+        @Test
         public void passwordIsOk(){
 
             Assert.assertTrue(Password_Checker.passwordIsOk());
         }
-    }
+
 }
